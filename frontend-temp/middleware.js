8795dfbfdaeb0724d90f9fae6c96d7621143ed38
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-06-10 20:54:21 +0530

    clerk redirect to /auh

--- frontend-temp/middleware.js
+++ frontend-temp/middleware.js
@@ -29,6 +29,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
+  '/auth(.*)',
   '/',
   "/welcome(.*)",
   "/prediction(.*)"
